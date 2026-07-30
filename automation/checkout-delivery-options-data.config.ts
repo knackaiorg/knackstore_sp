116717baaf5f87a58d328aedb9444341d100a840
jeevaknack <jeevakumar.s@knacksystems.com>
2026-07-30 16:55:02 +0530

    Added Data Config files Delivery, Stock badge, PDP

---
+++ automation/checkout-delivery-options-data.config.ts
@@ -0,0 +1,12 @@
+export class TestConfig {
+appURL="http://localhost:4200/"
+loginURL="http://localhost:4200/login"
+checkoutURL="http://localhost:4200/checkout"
+username="demo@knack.com"
+password="Demo@1234"
+freeDeliveryThreshold=200
+twoDayDeliveryFee="$ 20"
+nextDayDeliveryFee="$ 50"
+standardDeliveryFee="Free"
+}
+
---
+++ automation/pdp-product-q&a-recommendation-data.config.ts
@@ -0,0 +1,8 @@
+export class TestConfig {
+    appURL="http://localhost:4200/"
+    email="demo@knack.com"
+    password="Demo@1234"
+}
+
+
+
